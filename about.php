2787e80c2fbcf59b30fab2e262f7e8040ede09a2
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-20 20:15:38 +0200

    V0.6 About page styled and finished

--- about.php
+++ about.php
@@ -13,17 +13,17 @@
         include 'include/header.php'
     ?>
     <div class="container">
-        <div class="img">
-            <img src="images/Asset 1.png">
+        <div class="img1 img">
+            <img src="images/Asset 3.png">
         </div>
         <div class="text">
             <h1>Over mij</h1>
-            <p>Hallo, mijn naam is Sef. Ik ben 23 jaar en woon in Echt in het oh zo mooie Limburg. Ik hoop dat ik later een eigen design bureau kan oprichten.</p>
-            <p>Al sinds dat ik jong was heb ik altijd al een passie en talent gehad voor tekenen en ontwerp. Dit heeft een groot deel van mijn jeugd gevormd en het is ook altijd al mijn droom geweest om van deze passie ook mijn baan te maken.</p>
-            <p>Daarom heb ik ook voor deze opleiding gekozen om zo van mijn droom realiteit te maken. Sinds het begin van dit jaar ben ik enorm veel gegroeid in zowel development, leiderschap, onderzoek maar voor mij het belangrijkste: design.</p>
+            <p class="p1">Hallo, mijn naam is Sef. Ik ben 23 jaar en woon in Echt in het oh zo mooie Limburg. Ik hoop dat ik later een eigen design bureau kan oprichten.</p>
+            <p class="p2">Al sinds dat ik jong was heb ik altijd al een passie en talent gehad voor tekenen en ontwerp. Dit heeft een groot deel van mijn jeugd gevormd en het is ook altijd al mijn droom geweest om van deze passie ook mijn baan te maken.</p>
+            <p class="p2">Daarom heb ik ook voor deze opleiding gekozen om zo van mijn droom realiteit te maken. Sinds het begin van dit jaar ben ik enorm veel gegroeid in zowel development, leiderschap, onderzoek maar voor mij het belangrijkste: design.</p>
         </div>
-        <div class="img">
-            <img src="images/IMG_3577.png">
+        <div class="img2 img">
+            <img src="images/Asset 2.png">
         </div>
     </div>
 </body>
