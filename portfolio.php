ca3ce7a131d9ffed11dc98fb140366f43cdebe5c
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-23 20:15:53 +0200

    V0.10.1 Portfolio prototype page finished

--- portfolio.php
+++ portfolio.php
@@ -26,29 +26,40 @@
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2>Versie 1</h2>
-            <p></p>
+            <img src="images/portfolio 1.png" alt="portfolio 1">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2>Versie 2</h2>
-            <p></p>
+            <h2>Review 1</h2>
+            <p>Voor de 1ste review heb ik een prototype in XD gemaakt. Ik heb hierbij voor gekozen om alleen een kale layout te maken zonder styling. De styling kwam nog wel maar ik wilde me focussen op de layout.</p>
+            <p>Ik heb bij de 1ste versie gekozen om alle pagina's apart te maken, een keuze waar ik later op terug zou komen. Ook wilde ik een introductie pagina die heel simpel was om zo snel met mij kennis te maken, een pagina met alle leerdoelen en een about me pagina.</p>
+            <h3>Feedback peers</h3>
+            <p>Mijn groepsgenoten vonden dit prototype al erg goed eruit zien en ook makkelijk in gebruik. Ze vroegen me wel of ik nog iets ging doen met de stijl wat natuurlijk nog zou komen.</p>
+            <h3>Feedback leraren</h3>
+            <p>Ook docenten beviel het goed. Ik heb groen gekregen bij de 1ste review en ze vonden dat ik een goede manier had van noteren en ook voortgang te laten zien door veel iteraties te laten zien. Ook zeiden ze dat ik me voor de volgende review vooral moest focussen op het opvullen van mijn portfolio.</p>
+            <a href="https://xd.adobe.com/view/d44288c8-cc28-4529-ac9f-47fb3a3a67ee-8111/" class="button">Prototype 1</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2>Versie 3</h2>
-            <p></p>
+            <img src="images/yellow.png" alt="yellow">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Review 2</h2>
+            <p>Voor de 2e review heb ik niks veranderd aan de styling of layout van mijn portfolio en alleen content toegevoegd. Ik kreeg echter wel geel deze keer omdat ik nog niet de vertaalslag van prototype naar product had genaajt. Dit vond ik natuurlijk begrijpelijk en Bram, Frank en Sebastiaan zeiden ook dat voor de rest alles nog goed was.</p>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <img src="images/portfolio 2-3_1.png" alt="portfolio 2-3">
+        </div>
+        <div class="box">
+            <h2>Review 3</h2>
+            <p>Ik heb me tussen de 1ste en 2e review vooral bezig gehouden met de proftaak en heb hierdooreen beetje achterstand opgebouwd met het maken van mijn portfolio. Daarom heb ik besloten om snel alles te stylen aangezien ik genoeg materiaal had voor design. Hierbij heb ik 2 iteraties gemaakt.</p>
+            <h3>Feedback peers</h3>
+            <p>De 1ste iteratie beviel me zelf niet zo goed en a.h.v. van feedback van groepsgenoten en vrienden heb ik besloten om het helemaal opnieuw proberen te stylen.</p>
+            <p>Deze 2e iteratie vond ik nog steeds niet geweldig maar goed genoeg. Groepsgenoten en vrienden vonden het er erg netjes uitzien maar Joost Niessen (4e jaars FHICT student) raadde me aan om de UX te verbeteren door meer pagina's te verbinden met elkaar.</p>
+            <p>Dit advies heb ik ook opgevolgd en ik heb eerst de intro, leerdoel en about me pagina verbonden maar heb de about me pagina daarna los hiervan gehaald omdat ik deze niet belangrijk vond en hier wel altijd een link in de header voor is. Ook heb ik alle leerdoelen op 1 pagina gezet zodat er weinig gescrolled hoeft te worden. Hiermee heb ik de UX verbeterd.</p>
+            <a href="https://xd.adobe.com/view/d44288c8-cc28-4529-ac9f-47fb3a3a67ee-8111/" class="button">Prototype 2 (beware it's a mess)</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
@@ -63,7 +74,7 @@
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="portfolio website.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
