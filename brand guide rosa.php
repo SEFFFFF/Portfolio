f1498400c598b89f4390f37b763545e9421c9d4d
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 00:44:07 +0200

    V0.10.7 Finished brand guide page

--- brand guide rosa.php
+++ brand guide rosa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,55 @@
     </div>
     <div class="topbox">
         <h1>Brand Guide Rosa</h1>
-        <p></p>
+        <p>Met alle kennis die we opgedaan hebben via de mediaverkenning heb ik een brand guide samengesteld om zo de waardes van Rosa's merk te waarborgen. Deze brand guide heb ik vrijwel helemaal alleen gemaakt van het logo to alle beeldelementen.</p>
+        <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn2">Brand guide</a>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/bg layout.png" alt="layout">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Layout en inspiratie</h2>
+            <p>Als 1ste ben ik op Pinterest opzoek gegaan naar voorbeelden en inspiratie voor mooie brand guides. Uit het onderzoek van de mediaverkenning wist ik al wat de inhoud zou worden van de guide dus dat scheelde wel werk. Alle inspiratie heb ik verzameld op dit bord.</p>
+            <p>Met deze inspiratie heb ik een layout in Illustrator gemaakt dit was echt maar een heel ruwe schets en er zou nog veel veranderd worden maar de volgorde zou hetzelfde blijven.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/bg logo.png" alt="master page/logo">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Inhoud</h2>
+            <p>Dit was de 1ste keer voor mij dat ik met Indesign gewerkt heb. Ik heb me ingelezen over de functionaliteiten van dit programma en heb meteen een master page gemaakt als template voor alle pagina's. Dit werkte verassend goed. Zo kon ik ook snel pagina na pagina maken zodra alle content klaar was.</p>
+            <p>Het was soms een beetje lastig met fotos erin te krijgen omdat deze vaak van erg lage kwaliteit waren in Indesign. Dit kon ik verhelpen door de fotos als Illustrator bestand in te laden. Nadeel hiervan is wel dat de PDF een stuk groter is.</p>
+            <p>Tegelijkertijd dat ik hieraan bezig was heb ik ook de beeldelementen ontworpen die in deze brand guide gebruikt worden. Ook heeft iedereen in onze groep een paar logos gemaakt voor Rosa maar die van mij is uiteindelijk gekozen door haar.</p>
+            <p>Het kleurenpalet was door Rosa gekozen en de typografie heb ik uitgezocht en met de groep en Rosa besproken. Beide waren hier tevreden over.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/clap.png" alt="applause from comrade kim">
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Feedback</h2>
+            <p>Rosa was super blij met de brand guide. Ze vond alle elementen heel vet, vooral de gradients. Ook verschillende leraren gaven mij complimenten voor de vormgeving van dit document, ze zeiden dat dit professioneel uitzag en dat we een goede, unieke stijl hadden. Ook Michael en Jeroen van BrND zeiden dat we "een paar pareltjes" hiertussen hebben zitten en het er goed verzorgd uitziet.</p>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Ik heb met deze opdracht geleerd hoe ik Indesign kan gebruiken en de verschillende functionaliteiten hiervan zoals master pages, paragraph styles en margins.</p>
+        </div>
+        <div class="box">
+            <h2>Wat kon beter?</h2>
+            <p>Ook bij deze opdracht zou ik niks veranderen aan wat ik gedaan heb door de feedback maar ook mijn eigen gevoel.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="mediatechnieken.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
