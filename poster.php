87b60dcf9db0ce9a42d55ec0a5ff34db04baee78
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 12:40:41 +0200

    V0.10.11 Finished Poster page

--- poster.php
+++ poster.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,41 @@
     </div>
     <div class="topbox">
         <h1>Poster</h1>
-        <p></p>
+        <p>Als intro opdracht van dit semester heb ik een poster gemaakt over mezelf. Deze poster moet gaan over hoe je jezelf ziet over 20 jaar en metname de dingen die je dan bereikt wil hebben.</p>
     </div>
 </div>
 
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/poster.png" alt="poster">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <img src="" alt="">
+            <h2>Poster</h2>
+            <h3>Inspiratie</h3>
+            <p>Als 1ste ben ik begonnen met denken over wat voor verhaal ik wilde vertellen met mijn poster. Ik wist al snel dat dit een scene zou zijn waarin ik iets doe in het buitenland wat ook weer te maken heeft met mijn ontwerpen.</p>
+            <p>Met dit verhaal ben ik opzoek gegaan naar inspiratie. Ik kwam uiteindelijk bij de poster van Little Miss Sunshine. Ik vond de opmaak hiervan heel leuk en simpel. Een groot geel vlak met de personages van de film die naar een bus rennen. Dit wilde ik dus ook maar dan met mijn eigen twist.</p>
+            <h3>Opmaak</h3>
+            <p>Ik ben daarna begonnen aan het maken van een 1ste opmaak. Hiervoor heb ik geprobeerd een foto van mezelf erin te photoshoppen maar dit werkte niet goed. Ik bedacht me snel om dit gewoon te tekenen aangezien ik dat graag doe en deze poster ook over jezelf gaat.</p>
+            <p>Ik heb 2 afbeeldingen gepakt en deze overgetrokken en daarna mijn eigen aanpassingen eraan gegeven. Nu het beeld klaar stond hoefde ik alleen nog tekst toe te voegen.</p>
+            <p>Ik besloot om mijn film "one way flight to tokyo" te noemen omdat ik graag de wereld wil rondreizen en het liefste ook om design werk te doen. In het beeld van de poster zie je een getekende versie van mijzelf achter een japanse taxi aanrennen omdat ik ook nogal de neiging heb om te laat te komen voor dingen. Ook heb ik gekozen dat deze film door Tarantino (ik weet dat dit fout gespeld is Sebastiaan maar ik heb het PSD bestand niet meer dus ik kan dit niet aanpassen.) omdat dit mijn favoriete regisseur is.</p>
+            <h3>Feedback</h3>
+            <p>Ik kreeg een hoop complimenten voor mijn poster van zowel medestudenten als Leraren. Rick (medestudent) vertelde me dat als dit een echte film was hij deze door de poster wel wilde zien omdat deze interessant uit ziet. Sebastiaan en Evert vonden dit ook een sterke poster en vonden vooral het verhaal erachter leuk en ook vertellend over wie ik ben als persoon.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Dit was mijn 1ste keer dat ik ooit een poster gemaakt heb en al helemaal met het resultaat ben ik erg blij. Ik heb niet iets nieuws geleerd over Photoshop maar heb wel weer meer ervaring opgedaan door deze opdracht.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Ik zie niet iets wat ik echt anders zou doen bij deze opdracht. Ik heb de juiste stappen genomen om deze poster te realiseren en de uitkomst is ook goed.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="groepslogo.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
