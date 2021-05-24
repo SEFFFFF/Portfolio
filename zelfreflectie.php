0ef9cab69cf3a5e484a25af1cb89766f806e7fe5
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 17:25:06 +0200

    V0.10.19 Finished reflection page

--- zelfreflectie.php
+++ zelfreflectie.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,46 +19,33 @@
     </div>
     <div class="topbox">
         <h1>Zelfreflectie</h1>
-        <p></p>
+        <p>Voor proffesional identity heb ik een zelfreflectie geschreven met SWOT analyse van mezelf. Hierin zal ik beschrijven waar ik begonnen ben en hoe het nu verder zal gaan.</p>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/ik.png" alt="sef">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Zelfreflectie</h2>
+            <h3>Waar ben ik begonnen?</h3>
+            <p>Voor ik FHICT deed heb ik een lange tijd in de horeca gewerkt dus het was wel even schakelen voor mij vorig semester. Dit semester ging het al een stuk beter maar aan het begin had ik het gevoel dat ik nog steeds erg basis was qua development en ontwerpen.</p>
+            <p>Mijn grootste doel voor dit semester was mijn ontwerpen opkrikken en javascript leren. Met trots kan ik zeggen dat ik deze doelen gehaald heb en ik voel me vooral qua design heel zelfverzekerd.</p>
+            <h3>Hoe ging dit semester?</h3>
+            <p>Dit hele semester is my heel goed bevallen. Ik vond het heel fijn om eindelijk in een klas te zitten met mensen die hetzelfde als mij willen. Ook de opdrachten die we gemaakt hebben vond ik erg leuk om te doen.</p>
+            <p>Ik heb ook een hoop geleerd over dingen waar ik meer over wilde leren zoals het gebruik van Adobe programmas en hoe ik mijn web designs kan animeren.</p>
+            <p>Omdat ik zo veel plezier had aan design ben ik soms wel eens andere leerdoelen vergeten met name development maar dit was niet wat een weekje extra werken kon verhelpen.</p>
+            <h3>Waar ben ik goed in?</h3>
+            <p>Zoals ik al gezegd heb ben ik een heel stuk beter geworden in ontwerpen en kan ik ook een aantal Adobe programmas gebruiken die ik nog nooit gebruikt had. Ik heb op het gebied van ontwerpen ook een hoop geleerd door de kritische maar goede feedback van mensen zoals Joris en Bram.</p>
+            <p>Ik durf zelfs te zeggen dat ik qua ontwerpen nu op een niveau zit dat ik wellicht al een ZZP kan beginnen. A.h.v. feedback uit mijn omgeving en meerdere mensen die mij vragen om iets voor hun te maken na ze mijn werk zagen ben ik van plan om tijdens de zomer voor mezelf te beginnen. Deze vooruitgang is ook mijn trotste punt van dit semester.</p>
+            <h3>Waar moet mijn aandacht naartoe volgend semester?</h3>
+            <p>Hoewel ik de basis van Javascript geleerd heb denk ik dat ik nog steeds een hoop kan leren over development. Ik heb het gevoel dat dit steeds beter gaat maar nog steeds wil ik dit beter kunnen.</p>
+            <p>Ook andere vakken zoals research kunnen beter volgens mij. Ik heb hier al flinke stappen in gezet dit semester maar ik wil me hier komend semester meer op focussen omdat dit wel altijd de basis van een goed en relevant ontwerp is.</p>
+            <h3>Wat wil ik in de toekomst?</h3>
+            <p>Ik hoop dat ik ooit een eigen design bureau kan beginnen en branding opdrachten kan doen zoals ik die ook voor Rosa gemaakt heb. Ik vond dit soort werk ontzettend leuk vooral omdat dit een stuk kleinschaliger is dan een groot bedrijf, hier heb ik echt het gevoel dat mijn bijdrage impact heeft op de koers van een bedrijf/merk.</p>
+            <h3>SWOT analyse</h3>
+            <img src="images/swot.png" alt="png" class="swot">
         </div>
     </div>
 </div>
