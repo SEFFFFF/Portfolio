ae546ddeb4a420a0c6e93d80811c15cfb4077045
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 14:21:49 +0200

    V0.10.16 Finished apa page

--- apa opdracht.php
+++ apa opdracht.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,37 @@
     </div>
     <div class="topbox">
         <h1>APA opdracht</h1>
-        <p></p>
+        <p>Om te oefenen met de APA richtlijnen heb ik deze opdracht gemaakt.</p>
+        <a href="PDFs/APA2.pdf" class="back-btn3">Document</a>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/apa.png" alt="Plato's grot">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik gedaan?</h2>
+            <p>Voor deze opdracht mochten we kiezen uit 8 onderwerpen. Ik koos hier voor "Leiden internet en social media tot het einde van democratie?".</p>
+            <p>Mijn onderzoek begon door in Google de vraag te plakken. Hierdoor kwam ik al op verschillende artikelen uit. A.h.v wat ik hieruit las heb ik het onderzoek in elkaar gezet.</p>
+            <h3>Feedback</h3>
+            <p>De feedback hierop was gemengd. Zo vonden Huub en Berry dat het een interessant stuk was met hier en daar wat schrijf fouten en gaven ze me ook wat tips over bronvermelding. Nick vond dit ook een interessant stuk maar hij vond dat ik de APA richtlijnen niet goed had nageleefd. Zo zei hij dat ik te veel mijn eigen interpretatie opschreef en dit te weinig tot niet deed ondersteunen met bronnen.</p>
+            <p>Door deze feedback wist ik ook niet of ik dit wel in mijn portfolio wilde zetten maar na met Nick gesproken te hebben wilde ik dit toch doen. Hij gaf me het advies om de intercultural awareness opdracht te doen en deze opdracht te gebruiken als mijlpaal in voortgang voor het onderdeel research.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Ik heb geleerd om een stuk beter op mijn schrijfstijl en spelling te letten maar vooral dat tijdens onderzoeken ik niet mijn eigen opvattingen moet noteren maar alles onderbouwen doormiddel van bronnen, quotes, anecdotes en deze ook duidelijk in de tekst en literatuurlijst te vermelden.</p>
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <h2></h2>
+            <h2>Wat kon beter?</h2>
             <p></p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="opdrachtgever.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
