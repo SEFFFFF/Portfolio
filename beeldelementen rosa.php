b380e44b407408e40ae7e9ba45a4bb2fb0912ab1
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 11:47:36 +0200

    V0.10.9 Finished beeldelementen page

--- beeldelementen rosa.php
+++ beeldelementen rosa.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
 </head>
 <body>
@@ -19,51 +18,64 @@
     </div>
     <div class="topbox">
         <h1>Beeldelementen Rosa</h1>
-        <p></p>
+        <p>In de brand guide die ik voor Rosa gemaakt heb staan ook verschillende beeldelementen. Hier zal ik uitleggen hoe ik deze gemaakt heb.</p>
     </div>
 </div>
 
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/gradients.png" alt="gradients">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Gradients</h2>
+            <p>Gradients was een van de dingen wat het meeste terug kwam bij Rosa. Zowel in haar presentatie als in de stylescapes in de mediaverkenning waren gradients daar. Dit was ook een van de elementen die ik graag wilde uitwerken omdat we allemaal vonden dat dit perfect bij haar past.</p>
+            <p>Buiten dat het goed past bij Rosa was dit ook perfect om te animeren in latere sprints. Dit zouden goede achtergronden zijn die sfeer versterken of juist de toon geven voor een optreden, lied, filmpje, etc.</p>
+            <p>Eerst wilde ik deze gradients via Illustrator maken maar al snel merkte ik dat dit niet goed zou werken. De gradients die hieruit kwamen waren niet mooi en ook te veel geklooi om te maken in Illustrator. Ik ben opzoek gegaan naar goede guides om dit op te lossen en kwam al snel op eentje die dit deed in Photoshop.</p>
+            <p>Ik heb gekozen om dezelfde layout als Gabz aan te houden maar hier mijn eigen twist aan te geven door een ander Alien logo en foto te gebruiken. Feedback hierop was ook erg goed al raadde Joris me aan om voor een ander beeld en logo te kiezen om het zo nog meer kloppend te maken.</p>
+            <p>De gradients die hieruit kwamen waren perfect. Ook kon ik deze makkelijk customizen door het simpele proces van maken. Ik heb ook gekozen om een grain overlay te gebruiken dit geeft de gradient een oude vibe en geeft het wat extra zachtheid omdat de bedoeling hiervan nog steeds is om mensen een warm gevoel van nostalgie te geven.</p>
+            <h3>Feedback</h3>
+            <p>Zowel Rosa als leraren waren hier super tevreden over. Rosa vond dit perfect passen bij wat ze wil uitstralen en toen ze hoorde dat we deze ook gingen animeren was ze verkocht. Leraren vonden ze ook erg goed gelukt. Nick zei dat het hem aan vroeger deed denken dus mission accomplished.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/stickers.png" alt="stickers">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Stickers</h2>
+            <p>Als 2e element had ik gekeken naar de inspiratie borden die Rosa doorgestuurd had. Hier zag ik ook veel pop art invloeden, veel en felle kleurtjes, makkelijk reproduceerbaar en makkelijk uitbreidbaar.</p>
+            <p>Dit wilde ik graag ook ergens in verwerken om zo kleine elementjes te hebben die op veel manieren gebruikt konden worden en om paginas op te fleuren. Ik heb in Illustrator een paar vormpjes gemaakt die ik interessant vond en al snel kwam ik uit bij de sterretjes en ovalen. In deze vormpjes heb ik kernwaardes van Rosa's merk gezet maar dit zijn slechts placeholders. Deze kunnen gebruikt worden als titel aanduidingen of om belangrijke woorden te benadrukken.</p>
+            <h3>Feedback</h3>
+            <p>Deze stickers werden ook goed ontvangen. Rosa en leraren waren eerst een beetje verward door hoe deze gebruikt konden worden maar toen ik voorbeelden liet zien vonden ze best goed werken maar nog niet perfect.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/halo.png" alt="text halo">
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Text halos</h2>
+            <p>Als laatste element wilde ik iets doen wat het geheel een moderne twist geeft omdat de algemene stijl vrij klassiek is tot nu toe.</p>
+            <p>Ik heb hiervoor gekeken naar hedendaagse design trends en hierbij zag ik 3D ringen met tekst erin. Dit vond ik heel vet dus ik heb een tutorial gezocht over hoe je dit kon maken en dat bleek ook nog eens heel makkelijk te zijn.</p>
+            <p>Het maken van deze halos was erg makkelijk en met het resultaat ben ik ook heel blij. Deze zijn echt bedoeld om gebruikt te worden in combinatie met andere vormen en gradients. Zo kun je de pagina coole 3d effecten geven door b.v. de ring achter een object te laten verdwijnen.</p>
+            <h3>Feedback</h3>
+            <p>Rosa en leraren waren hier weer erg tevreden over en vonden dit ook goed passen bij het geheel. Ook vonden ze het idee om deze later te animeren erg cool.</p>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Ik heb hier geleerd om informatie en inspiratie om te zetten tot ontwerpen en elementen. In het begin vond ik dit best lastig omdat ik bang was dat ik Rosa's bedoeling fout zou begrijpen maar gelukkig sloeg ik de spijker op zijn kop.</p>
+        </div>
+        <div class="box">
+            <h2>Wat kon beter?</h2>
+            <p>Hoewel Rosa, docenten en ikzelf erg tevreden zijn over deze elementen denk ik dat ze nog beter had kunnen maken als ik meer tijd had.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="mediaverkenning.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
