87760f78fc8a6ff9df87693c4cc4a54cc558b38b
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 13:41:46 +0200

    V0.10.13 Finished IA page

--- intercultural awareness.php
+++ intercultural awareness.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,49 @@
     </div>
     <div class="topbox">
         <h1>Intercultural awareness</h1>
-        <p></p>
+        <p>Als 2e research opdracht hebben we a.h.v. Hofstede Insights een land moeten onderzoeken. Dit onderzoek moesten we uitbeelden met beelden die bij een bepaalde cultuurdimensie hoort.</p>
+        <a href="PDFs/Intercultural Awareness_V2.pdf" class="back-btn3">Document</a>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/ia1.png" alt="IA1">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Versie 1</h2>
+            <p>Ik wist al meteen dat ik deze opdracht over Japan zou doen aangezien dit land mij altijd al interesseert en inspireert.</p>
+            <p>Bij de 1ste versie had ik de opdracht fout begrepen. Het was de bedoeling om 3 beelden bij 1 cultuurdimensie te pakken maar ik heb 3 dimensies met ieder 1 beeld gemaakt. Daarom had ik gekozen voor Insecurity, Masculinity en Individualism. Hier heb ik me goed aan de APA regels gehouden qua gebruik van beelden maar niet echt bij de informatie. Ook heb ik ervoor gekozen om deze opdracht in het engels te doen. Ik heb het gevoel dat ik veel beter verhalen kan vertellen in het engels.</p>
+            <h3>Feedback</h3>
+            <p>De feedback van Berry en Huub was ook dat dit een leuk werkstuk is maar dat de APA stijl beter gehanteerd kan worden door in mijn tekst ook meer te verwijzen naar bronnen en in de inleiding/conclussie mijn eigen verbinding met japan beter kon leggen. Ook werd ik erop gewezen dat ik de opdracht fout heb begrepen dus ik had wel een aantal punten waar ik aan moest werken.</p>
+            <a href="PDFs/IA.pdf" class="back-btn3">Versie 1</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/ia2.png" alt="IA2">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Versie 2</h2>
+            <p>Bij de 2e versie heb ik gekozen om het alleen over Insecurity/Avoidance te houden omdat Japan hier een 92 op scoort. Dit heb ik ook correct met 3 beelden en bijschrift geillustreerd. In de bijschriften heb ik de APA stijl beter gehanteerd en bij ieder stukje informatie een bron gegeven. Ook heb ik mijn eigen fascinatie voor Japan beter genoteerd.</p>
+            <H3>Feedback</H3>
+            <p></p>
+            <a href="PDFs/Intercultural Awareness_V2.pdf" class="back-btn3">Versie 1</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Deze opdracht wilde ik vooral maken omdat de eerdere APA opdracht niet zo goed gelukt was. Ik heb goed geluisterd naar de feedback van deze en vorige opdracht. Ik vond het eerst best lastig om alles goed via APA stijl te doen maar daar heb ik deze keer meer over geleerd door meteen de bron over te nemen en een correcte bronvermelding te maken.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Ik heb op deze opdracht nu nog geen verbeter punten omdat ik nog wacht op de feedback van versie 2.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="branding.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
