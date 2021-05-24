57c04e4f9425f7385145c7135600463e6c8d9461
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 11:18:57 +0200

    V0.10.8 Finished mediatechnieken page

--- mediatechnieken.php
+++ mediatechnieken.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,73 @@
     </div>
     <div class="topbox">
         <h1>Mediatechnieken</h1>
-        <p></p>
+        <p>Als 2e specialisatie opdracht heb ik gekozen voor de mediatechnieken opdracht. Van deze opdracht heb ik de Photoshop, Indesign, Premiere Pro en After Effects opdracht gedaan.</p>
+        <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn3">Uitgebreid verslag</a>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/posters.png" alt="posters">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Posters</h2>
+            <p>Voor het 1ste gedeelte heb ik 6 posters a.h.v. 3 plex card waardes maken. Ik heb hierbij gekozen voor de waardes: Exploration, Fantasy en Sensation. Hierbij heb ik per waarde 2 ontwerpen gemaakt en deze verbeterd a.h.v. feedback van peers en docenten.</p>
+            <p>De reactie op deze posters waren erg positief. Mijn peers vonden ze erg goed eruit zien en zagen dat ik er de tijd voor had genomen. Joris vond ze ook vet alleen raadde hij me aan om wat meer variatie te doen en beter te letten op de voorwaarde dat alle poster zwart/wit + 1 steun kleur moeten zijn. Hiernaast zie je hoe ik 1 poster heb aangepast.</p>
+            <p>Als 2e deel heb ik een reposter van mijn favoriete film gemaakt. Ik heb gekozen voor de horror klassieker "Alien" omdat dit een van mijn favoriete films allertijde is. Eerst ben ik opzoek gegaan naar inspiratie. Zo had ik een mooie poster van Gabz gevonden waar ik veel inspiratie van kreeg.</p>
+            <p>Ik heb gekozen om dezelfde layout als Gabz aan te houden maar hier mijn eigen twist aan te geven door een ander Alien logo en foto te gebruiken. Feedback hierop was ook erg goed al raadde Joris me aan om voor een ander beeld en logo te kiezen om het zo nog meer kloppend te maken.</p>
+            <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn3">Posters</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/spreads.png" alt="spreads">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Bladspiegels</h2>
+            <p>Voor de 2e opdracht heb ik 10 bladspiegels gemaakt in Indesign. Hiervoor mochten we 10 onderwerpen kiezen die ons inpsireren/intresseren en deze op een mooie, informatieve en experimentele vorm presenteren.</p>
+            <p>Ik heb gekozen voor de onderwerpen: Vietnam, Iwo Jima, The Weather Project, Hokusai, Akira, Halo 3, Gorillaz, Daft Punk, Blade Runner 2049 en Moebius. Het maken van deze bladspiegels ging soepel en snel omdat ik dit al vaker gedaan heb dit semester en met de resultaten ben ik erg tevreden. Hiernaast staan 2 van mijn favoriete spreads uit dit document.</p>
+            <p>Dit vond ik de leukste opdracht eruit omdat het me ook een hoop nieuwe ervaring heeft gegeven. Ik voel me nu heel comfortabel met Indesign en denk dat ik ook op veel verschillende manieren informatie in een mooie, leuke en interessante manier kan brengen.</p>
+            <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn3">Spreads</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/premiere.png" alt="premiere">
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Premiere Pro</h2>
+            <p>Als 3e opdracht heb ik voor de 1ste keer met Premiere Pro een chroma key video gemaakt waarin we iets over onszelf vertellen en een instagram story op de beat van muziek.</p>
+            <p>Zoals ik al zei is dit de 1ste keer voor mij dat ik ooit iets gedaan heb met filmen en bewerken van videos dus dit was nogal een uitdaging voor me. Het leren van Premiere viel heel erg mee en ik vond dit erg makkelijk te gebruiken. Het filmen van alles bleek het grootste probleem te zijn omdat ik 0 verstand had van hoe ik mooie shots kon krijgen.</p>
+            <p>Uiteindelijk na wat guides gekeken te hebben is dit me allemaal gelukt en was ik blij verrast met het resultaat. Joris vond dit ook voor een 1ste keer al erg netjes al vond hij wel dat het chroma key effect beter kon en de story wat beter op de beat geedit kon worden.</p>
+            <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn3">Videos</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <img src="images/AE.png" alt="after effects">
+        </div>
+        <div class="box">
+            <h2>After Effects</h2>
+            <p>Als laatste opdracht heb ik in After Effects een keyframe video van mezelf in een andere omgeving en een 3d tracking video gemaakt. Ook dit was voor mij de 1ste keer dus ik heb een hoop nieuwe dingen moeten leren.</p>
+            <p>Ik vond After Effects een stuk lastiger in gebruik dan Premiere door de ontelbaar vele functionaliteiten van dit programma. Maar na een hoop tutorials te kijken lukte dit wel. Ook hier ondervond ik weer het probleem dat ik het lastig vind om goede footage te krijgen voor deze opdracht. Zo heb ik vooral bij de tracking opdracht zeker 10 keer opnieuw moeten filmen.</p>
+            <p>Ook hier was ik best blij met het resultaat. Joris vond het leuk dat ik bij de keyframe opdracht mezelf in een auto gezet had maar zei dat het nog beter zou zijn als de auto bewoog en ik betere color correction op mijn cutout had gedaan om het zo overtuigender uit te laten zien.</p>
+            <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn3">Videos</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Met deze opdracht heb ik voor het eerst kennis gemaakt met Premiere en After Effects. Van beide ken ik nu de basis en hier wil ik in de toekomst ook zeker op uitbreiden. Ook heb ik weer nieuwe layouts gemaakt in Indesign, deze zullen ook zeker van pas komen.</p>
+        </div>
+        <div class="box">
+            <h2>Wat kon beter?</h2>
+            <p>Met deze opdracht heb ik voor het eerst kennis gemaakt met Premiere en After Effects. Van beide ken ik nu de basis en hier wil ik in de toekomst ook zeker op uitbreiden. Ook heb ik weer nieuwe layouts gemaakt in Indesign, deze zullen ook zeker van pas komen.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="beeldelementen rosa.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
