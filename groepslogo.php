4a59300bbf4ebf354737ca2785dcc903a4639424
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 12:56:24 +0200

    V0.10.12 Finished logo page

--- groepslogo.php
+++ groepslogo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,46 +19,39 @@
     </div>
     <div class="topbox">
         <h1>Groepslogo</h1>
-        <p></p>
+        <p>Omdat we voor de proftaak in groepen gewerkt hebben leek het onze groep het een leuk idee om onze groep ook te branden. Hiervoor heb ik snel een logo ontworpen.</p>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/chili ite.png" alt="iteraties">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Iteraties</h2>
+            <p>Als eerste hebben we gezamenlijk een naam besproken. Na een hoop heen en weer gaan met namen kwamen we uit bij CHILI, dit vonden we dat lekker over de tong rolde dus we besloten dit te gebruiken met de slogan: Lets spice up your brand!</p>
+            <p>Met deze naam heb ik een hoop verschillende iteraties gemaakt. Ik wilde gaan voor een moderne en snelle vibe. CHILI moet bij de tijd zijn. Na een hoop experimenten kwam ik bij een vlag versie die ik wel interessant vond. Deze heb ik uitgewerkt.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/chili fin.png" alt="finished">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Eindversie</h2>
+            <p>Zoals gezegd heb ik de vlag versie verder uitgewerkt. Ik heb deze eerst doorgesneden en nog outline toegevoegd maar het leek me vetter om dit outline effect te benadrukken. Dit effect heb ik verdubbeld en een beetje offset van het logo gedaan. Dit geeft het een cool 3D effect. Uiteindelijk heb ik maar kort gewerkt aan dit logo omdat we vonden dat dit geen prioriteit was maar een leuke extra.</p>
+            <h3>Feedback</h3>
+            <p>Het logo werd goed ontvangen door docenten en studenten. Ze vonden het mooi en modern uitzien. Ik kreeg wel van verschillende mensen te horen dat de zichtbaarheid van het outline effect slecht te zien is als het logo klein is. Ik heb ervoor gekozen om dit niet te veranderen aangezien het logo niet vaak gebruikt wordt, dit geen prioriteit is en als het gebruikt wordt is dat 90% van de tijd in een groot, leesbaar formaat.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Ik heb met het maken van dit logo niet echt iets nieuws geleerd. Ik vond het wel fijn om te oefenen met itereren en uit te breiden op iteraties.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Volgende keer pak ik me genoeg tijd voor het maken van een logo en ga ik ook meer kijken naar de leesbaarheid hiervan.</p>
         </div>
     </div>
 </div>
