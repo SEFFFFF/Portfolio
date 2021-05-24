f2608e09cf36a1f06280f02f6ea246c3768ed354
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 13:56:11 +0200

    V0.10.14 Finished doom branding page

--- branding.php
+++ branding.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,41 @@
     </div>
     <div class="topbox">
         <h1>Branding</h1>
-        <p></p>
+        <p>Als 1ste specialisatie opdracht heb ik gekozen voor branding. Ik wilde deze opdracht doen omdat ik de branding van de proftaak heel leuk vind en ik meer ervaring wil opdoen met andere methodes dan de 24PLUSONE methode.</p>
+        <a href="PDFs/Branding Doom Verslag.pdf" class="back-btn3">Uitgebreid verslag</a>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/doom brand.png" alt="brand guide">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <img src="" alt="">
+            <h2>Wat heb ik gedaan?</h2>
+            <p>Voor deze opdracht kregen we verschillende merkonderzoekstools aangerijkt. Hier heb ik kort onderzoek naar gedaan en de keuze gemaakt om de brand canvas te gebruiken.</p>
+            <p>Deze brand canvas zou ik wel vrij beknopt moeten houden want de inhoud mag maximaal uit 4 a4'tjes bestaan. Als ik alle onderdelen in dit document moet stoppen zou dit of heel romelig zijn, of zo beknopt dat het alle nuance verliest.</p>
+            <p>Met de tool die ik gekozen heb ben ik gaan kijken naar een goed onderwerp voor deze opdracht. Bij de voorbeelden stond Call of Duty: Modern Warfare, hier ben ik zelf niet zo fan van maar ik moest wel meteen aan Halo denken.</p>
+            <p>Dit onderwerp heb ik aan Sebastiaan en Evert voorgelegd en hun vonden dit een goed onderwerp dus hier ben ik onderzoek naar gaan doen.</p>
+            <p>Al snel kwam ik met mijn onderzoek op de conclusie dat Halo een te brede franchise is met te veel verschil in stijl tussen titels en developers. Dit vond ik jammer maar is ook niet erg want daar is onderzoek ook voor, om er ook achter te komen dat iets niet kan.</p>
+            <p>Gelukkig had ik al een backup onderwerp, Doom Eternal. Hiervoor heb ik verschillende bronnen geraadpleegd. Zo heb ik natuurlijk het internet afgezocht naar informatie, ik heb zelf al een boek over het ontwerp van dit spel dus dat helpt ook maar gelukkig is ID software heel transparant in hun development proces en er staan vele uren beeldmateriaal en interviews op Youtube.</p>
+            <h3>Feedback</h3>
+            <p>Na de brand guide af was heb ik deze aan Sebastiaan en Evert laten zien en hun hadden heel goede feedback hierover gegeven. Zo zeiden ze dat ze weinig aan te merken hadden over de inhoud en vonden ze dat met de hele stijl van deze brand guide hun meenam in wat het Doom merk wil uitstralen.</p>
+            <a href="PDFs/Branding Doom.pdf" class="back-btn3">Brand guide</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Na de brand guide af was heb ik deze aan Sebastiaan en Evert laten zien en hun hadden heel goede feedback hierover gegeven. Zo zeiden ze dat ze weinig aan te merken hadden over de inhoud en vonden ze dat met de hele stijl van deze brand guide hun meenam in wat het Doom merk wil uitstralen.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Ik had beter de APA stijl kunnen hanteren betreft het gebruik van plaatjes en informatie maar dat was voor deze opdracht erg lastig omdat ik vooral bij de informatie juist mijn eigen invulling moest doen. Er was vrijwel geen materiaal wat precies hun kernwaardes en stijl beschrijft.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="interview Rosa.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
