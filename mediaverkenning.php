d5507ed5602ac3b2eaca14b02fa52a4ef0e2eac3
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 12:15:23 +0200

    V0.10.10 Finished mediaverkenning page

--- mediaverkenning.php
+++ mediaverkenning.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,51 @@
     </div>
     <div class="topbox">
         <h1>Mediaverkenning</h1>
-        <p></p>
+        <p>Als 1ste opdracht voor Rosa moesten wij onderzoek doen naar wie zij is, wat haar plaats is en hoe wij haar kunnen helpen en dit onderzoek in een professioneel vormgegeven mediaverkenning te verwerken.</p>
     </div>
 </div>
 
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/layout media.png" alt="layout">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Layout</h2>
+            <p>De styling en lay out van de mediaverkenning heb ik op me genomen. Ik wilde graag leren hoe ik een mooi boekje kan maken. Als eerst heb ik natuurlijk inspiratie gezocht op Pinterest. Dit heb ik allemaal verzameld in een bord</p>
+            <p>Met deze inspiratie en de rest van de groep heb ik een layout document opgesteld. Hierin zou ik snel de layout schetsen en dit met de rest van de groep bespreken. We kwamen al snel bij een goed lopend verhaal uit echter zou er nog veel veranderen tijdens het maken van dit document.</p>
+            <p>Het opstellen van deze layout was erg simpel en heeft ook niet veel tijd gekost.</p>
+            <a href="https://pin.it/44qTqo1" class="back-btn3">Pin bord</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/voorpagina.png" alt="voorpaginas">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Opmaak</h2>
+            <p>Nu ik de layout af had was het tijd om het document echt te maken. Dit bleek meteen een stuk moeilijker te zijn dan ik dacht. Ik had geen idee wat voor stijl ik zou geven aan het geheel dus heb besloten om eerst een aantal voorpaginas te maken en hier een keuze uit te maken over hoe de rest van het document eruit komt te zien.</p>
+            <p>Na een hoop experimenteren kwam ik bij een cover die ik cool vond. Deze heb ik nog verder uitgewerkt en gepresenteerd aan Joris, Bram en Berry. Hun vonden dat deze cover er al goed uitzag maar ze waren vooral blij met het iteratief proces wat ik doorlopen heb voor deze opdracht. Berry raadde me aan om de ligging van het blad te veranderen. Dit maakte het meteen een stuk interessanter en zo kon ik content ook makkelijker indelen.</p>
+            <p>Met deze feedback heb ik de rest van het document gestijld in de stijl van de cover. Dit bleek ook nog eens erg lastig te zijn omdat het veel paginas waren en ik na 15 paginas inspiratieloos begon te raken. Het is me uiteindelijk toch gelukt.</p>
+            <p>Voor het maken van dit document heb ik Illustrator gebruikt omdat ik hier het meeste ervaring mee had. Dit was echter geen slimme keuze wat ik later zou leren toen ik Indesign ging gebruiken. Door het gebruik van Illustrator heeft het maken van dit document veel langer geduurd dan nodig was en heb ik een hoop tijd verspild.</p>
+            <h3>Feedback</h3>
+            <p>Rosa en leraren waren onder de indruk van het document wat we geleverd hebben. Ze vonden het compleet qua onderzoek, goed/professioneel vormgegeven en ook de plannen voor toekomstige sprints kwamen goed over.</p>
+            <a href="https://github.com/SEFFFFF/Text-animations" class="back-btn3">Mediaverkenning</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Met de mediaverkenning heb ik een hoop geleerd over het stylen van een booklet. Dit heb ik hiervoor nog nooit gedaan en dit was ook een leuke ervaring.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Volgende keer dat ik weer een boekje ga maken ga ik Indesign gebruiken. Hiermee kan ik mezelf een hoop tijd en onnodig werk besparen.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="poster.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
