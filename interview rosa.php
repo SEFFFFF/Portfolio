f38ae08f72734ea1e1176dcdbecda7ad2d29a835
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-05-24 14:12:59 +0200

    V0.10.15 Finished interview page

--- interview rosa.php
+++ interview rosa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="temp dev.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Document</title>
@@ -19,51 +19,49 @@
     </div>
     <div class="topbox">
         <h1>Interview Rosa</h1>
-        <p></p>
+        <p>Om een beter beeld te krijgen over wie Rosa is, wat haar doel is, hoe ze zich wilt uiten en vooral hoe wij haar kunnen helpen hebben we een interview gehouden met haar</p>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/rosa.png" alt="rosa">
         </div>
         <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <img src="" alt="">
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Vragen</h2>
+            <p>Als eerste zijn we als groep begonnen met vragen te bedenken die we konden stellen, hierbij hielden we constant de onderzoeksvraag, Wat willen wij weten van Rosa wat ons kan helpen, in ons achterhoofd.</p>
+            <p>Na een hoop brainstormen hebben we een mooi document samengesteld met uitgebreide vragen. Ook hebben we gekozen hoe we de 23plusone methode gingen gebruiken.</p>
+            <h3>Feedback</h3>
+            <p>Nadat we een voortgangsgesprek hadden met Michael waarin hij ons goed advies gaf om dit interview samen met andere groepen te doen, heb ik contact gezocht met de andere groepsleiders om zo een bespreking te plannen.</p>
+            <a href="PDFs/Vragen Rosa.pdf" class="back-btn3">Vragenlijst</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/plusone.png" alt="23plusone">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Interview</h2>
+            <p>Het interview ging heel goed, Luoxin en ik hadden goed contact met Rosa en konden ook lekker luchtig met haar praten. Waarintegen de rest van de mensen vrijwel niks gezegd hebben.</p>
+            <p>Na de vragen hebben we Rosa gevraagd om de pyramide in te vullen aan de hand van de 23plusone methode. (zie hiernaast)</p>
+            <h3>Transciptie</h3>  
+            <p>Het hele interview is ook getranscribeerd door onze groep, hier was eerst lichtelijke weerstand tegen maar toen we het toch gebruikte begrepen andere groepsgenoten wel waarom we deze moeite genomen hebben.</p>
+            <a href="PDFs/Transcriptie interview Rosa.pdf" class="back-btn3">Transcriptie</a>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Voor deze opdracht heb ik nog nooit een interview gehouden dus dit heeft me vooral hier dingen over geleerd. Zo merkte ik dat het echt ontzettend hielp om alle vragen klaar te hebben in een bestand.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Iets wat minder leuk is wat ik hier ook heb geleerd is om niet andere mensen te laten meeliften op jouw werk als ze zelf niks hieraan toevoegen. In de toekomst ga ik dit ook niet meer doen.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="apa opdracht.php" class="next">Volgende project</a>
 </div>
 <script src="script.js"></script>    
 </body>
