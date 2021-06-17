b4543c2cfb4d85bcabdf910554ff5abaee5ab247
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-06-17 16:05:32 +0200

    V1.4 Added sprint X project page

--- sprint x project.php
+++ sprint x project.php
@@ -19,51 +19,114 @@
     </div>
     <div class="topbox">
         <h1>Sprint X project</h1>
-        <p></p>
+        <p>Als klapper op de vuurpijl voor dit semester hebben we in sprint X 3 weken de tijd gekregen om onze entrepreneur of research skills te laten zien.</p>
+        <a href="PDFs/SPRINT X_v1.1.pdf" class="back-btn3">Uitgebreid verslag</a>
     </div>
 </div>
-
 <div class="container">
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <h2></h2>
-            <p></p>
+            <img src="images/joris.png" alt="communicatie joris">
         </div>
         <div class="box">
-            <img src="" alt="">
+            <h2>Idee</h2>
+            <p>Ik wist meteen toen we de briefing kregen wat ik wilde gaan doen, dit idee had ik namelijk al een tijdje in mijn hoofd. Ik wilde mijn eigen kledingmerk beginnen. Zoals ik al zei had ik dit idee langer maar dit leek me de perfecte mogelijkheid om dit ook te combineren met school.</p>
+            <p>Dit idee heb ik uitgewerkt in een sprint plan en een begeleider gekozen en benaderd. Ik heb gekozen voor Joris Ariëns omdat ik hier al vaker contact mee heb gehad dit jaar en ik altijd goede feedback van hem krijg op het gebied van ontwerpen.</p>
+            <p>Het was mijn plan om deze sprint een uitgebreid onderzoek doen naar ondernemerschap en mijn eigen merk (concurrentie, branding, doelgroep). Verder wilde ik nog een goed logo maken. Om ICT in dit project te verweven wilde ik ook nog een prototype webshop maken. Als laatste was de opdracht om een promotie video te maken van je project van maximaal 75 seconden.</p>
+            <a href="PDFs/sprint X plan.pdf" class="back-btn4">Sprint plan</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">        
+        <div class="box">
+            <img src="images/onderzoek.png" alt="onderzoek voorbeeld">
+        </div>
+        <div class="box">
+            <h2>Onderzoek</h2>
+            <h3>Library research</h3>
+            <p>Als eerste begon ik met het onderzoeken wat het inhoud om een ondernemer te zijn. Ik las het artikel wat op canvas stond maar hier kon ik niet veel nuttige informatie uithalen. Gelukkig kwam ik al snel bij een artikel van KVK wat per stap beschrijft wat je nodig heb om een bedrijf te beginnen. Hier heb ik me de tijd voor genomen om dit uit te werken.</p>
+            <p>De stappen waren erg handig en maakte alles snel duidelijk. Het uitvoeren van deze stappen duurde wel even omdat dit voor een groot deel bestond uit het maken van een business plan. Toen ik dit plan bijna af had heb ik aan een goede vriend van mij laten zien die hier ervaring mee heeft en hij zei tegen mij dat dit niet perse nodig is aangezien ik een eenmanszaak wordt die geen investeerders of leningen nodig heeft. Joris vertelde me ook hetzelfde.</p>
+            <p>Hierna heb ik onderzocht wie de concurrentie was in Nederland, deze waren: Daily Paper, Olaf Hussein, Bonne Suits. Nu ik wist wat de concurrentie was en hoe ik mezelf hiervan kon onderscheiden heb ik onderzoek gedaan naar wat mijn doelgroep was. Dit was best lastig aangezien dit merk nog niet bestaat maar dit geeft tegelijkertijd wel vrijheid omdat je kan kiezen welke doelgroep je wilt bereiken. Ik kwam uiteindelijk op jongeren van 16-27 die meer van de alternatieve kant zijn.</p>
+            <h3>Field research</h3>
+            <p>Toevallig zit er in onze klas een jongen, Soulaiman Meskini, die al een eigen merk heeft dus het leek me perfect om hem te benaderen en wat vragen te stellen. Ik heb met hem een dik uur gesproken over hoe je het beste je product kan marketen (instagram, influencers benaderen en weten wie je doelgroep is), wat goede manieren van productie zijn (hij gebruikt een print on demand service) en hoe je klanten behoud (marketing na aankoop van een product en interactie met je klanten). </p>
+            <p>Het volledige onderzoek staat in het uitgebreide verslag.</p>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">      
+        <div class="box">
+            <img src="images/scotlogo.png" alt="logo iteraties">
+        </div>
+        <div class="box">
+            <h2>Logo</h2>
+            <p>Het logo is een lang proces geweest maar wel eentje die nodig was en ook zijn vruchten heeft afgeworpen. Ik wilde een logo maken dat zowel skaters als mensen die Daily Paper dragen aanspreekt dus ik ben meteen begonnen met iteraties maken.</p>
+            <h3>Versie 1</h3>
+            <p>Als 1ste versie heb ik gekozen voor een schuine, schreefloze display letter om het zo een snel en modern gevoel te geven. Dit heb ik gekoppeld met een onderschrift met services en jaartal. Dit was nog te gewoontjes en ook te netjes.</p>
+            <h3>Versie 2</h3>
+            <p>Bij de 2e versie heb ik het middenstuk en de strepen van het onderschrift vervangen met een clipart ster. Hiermee wilde ik het een old school gevoel geven wat wel strak en stijlvol is. Hierop kreeg ik goede feedback van Joep, hij vond het al erg mooi uitzien met de ster maar raadde me aan om er nog een beeld bij te maken om het zo makkelijker herkenbaar te maken.</p>
+            <h3>Versie 3</h3>
+            <p>De 3e versie heeft daarom ook een rennend figuur erbij. Mijn insteek hierbij was omdat de term "scot free" betekend om van iets weg te komen zonder straf, dat dit figuur ook weg rent voor iets. Verder heb ik ook het font vervangen door een recht, schreefloos font wat net wat meer futuristisch is, dit heb ik vooral gedaan omdat de schuine letter de logo lockup uit balans bracht. Deze keer heb ik het logo aan verschillende leraren gepresenteerd, de feedback hierop was dat het er al erg goed uitzag. Verder vroeg Berry zich af of het figuur wel nodig was, Bram en Joris zeiden dat het logo te netjes was en beter bij mijn doelgroep kon passen.</p>
+            <h3>Versie 4</h3>
+            <p>Daarom heb ik bij de laatste versie gekozen om het logo te houden bij een lettermerk en me hierop te focussen. Ik heb gekozen voor een versimpelde blackletter die het een rebels maar high end gevoel geeft omdat deze niet te overdreven is maar ook niet te zacht. Feedback hierop was erg goed, zo kreeg ik meteen van verschillende klas genoten en vrienden de vraag wanneer ze iets konden bestellen.</p>
+            <a href="PDFs/scotlogoite.pdf" class="back-btn4">Iteraties</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">      
+        <div class="box">
+            <img src="images/scotproto.png" alt="prototype">
+        </div>
+        <div class="box">
+            <h2>Prototype</h2>
+            <p>Omdat dit natuurlijk HBO ICT is wilde ik me ook focussen op het maken van een prototype voor de webshop.</p>
+            <h3>Inspiratie</h3>
+            <p>Ik heb eerst gekeken naar andere merken en hoe hun de website maken. Al snel viel me op dat een hoop webshops van bekende merken vrijwel hetzelfde uitzien. Dit was voor mij genoeg reden om ervoor te kiezen om de webshop van Daily Paper en Olaf te emuleren.</p>
+            <h3>Schetsen</h3>
+            <p>Na de inspiratie heb ik snel een paar wireframe schetsen gemaakt op papier. Hierdoor had ik een beter idee over hoe ik het wilde aanpakken.</p>
+            <h3>Versie 1</h3>
+            <p>Als laatste heb ik de schetsen tot een volwaardig clickable prototype uitgwerkt. Dit ging allemaal heel snel omdat ik veel voorbeelden had van andere merken maar ik heb zo veel mogelijk die van Olaf aangehouden omdat deze me het meeste inspireerde. Hierdoor heb ik ook maar verschillende minimale dingen veranderd aangezien het niet nodig was om volledig uniek te zijn.</p>
+            <h3>Feedback</h3>
+            <p>Ik heb hier van verschillende vrienden en klasgenoten feedback op gekregen en hun 1ste reactie was altijd "wow". Ze vonden de site heel fijn om te navigeren en als ze naar de looks keken kregen ze een bijenkorf gevoel. Het enige wat sommige zeiden is dat de blauwe balk misschien afleidend wordt maar niet iets wat hun ervaring minder maakt. Door deze feedback heb ik ook besloten om niks meer te veranderen aangezien ik zelf ook uiterst tevreden was over de site.</p>
+            <a href="https://xd.adobe.com/view/f2ed8a46-f7a6-4708-899c-bddb8e6b6388-a7be/" class="back-btn4">Prototype</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">      
+        <div class="box">
+            <img src="images/promovid.png" alt="video">
+        </div>
+        <div class="box">
+            <h2>Video</h2>
+            <p>Als laatste onderdeel heb ik een promotie video gemaakt met verschillende mockups en beelden die passen bij het merk.</p>
+            <h3>Inspiratie</h3>
+            <p>Gedurende dit semester heb ik vaker op youtube naar kleine merken gekeken en hoe hun videos maken. Eentje wat mij erg aansprak was Global Mind Studios door zijn stijlvolle videos waarin hij meestal timelapses van ontwerpen maakt en deze doet showcasen daarna. Ik kon helaas geen timelapse meer maken aangezien ik alles al gemaakt en klaar had dus ik heb ervoor besloten om hierin de website en de mockups te showcasen en me vooral te focussen op het beeld.</p>
+            <h3>Uitvoering</h3>
+            <p>Ik heb gezocht naar gratis stock footage van california, skaters en palmbomen omdat dit toepasselijk bij mijn merk is. Deze beelden heb ik aan elkaar gemonteerd en met een tutorial op youtube een cool 8mm film effect erop toegevoegd. Hierna hoefde ik alleen maar de producten in de video te doen en klaar was kees. Het maken van deze video duurde redelijk lang omdat ik erg perfectionistisch was maar voor de rest ging dit heel soepel door de handige tutorial.</p>
+            <h3>Feedback</h3>
+            <p>Toen we allen onze videos woensdag gepresenteerd hadden kreeg ik daarna verschillende complimenten van medestudenten en weer vragen wanneer de 1ste release is. Joris vond de video ook erg cool </p>
+            <h3>Feedback</h3>
+            <p>Ik heb hier van verschillende vrienden en klasgenoten feedback op gekregen en hun 1ste reactie was altijd "wow". Ze vonden de site heel fijn om te navigeren en als ze naar de looks keken kregen ze een bijenkorf gevoel. Het enige wat sommige zeiden is dat de blauwe balk misschien afleidend wordt maar niet iets wat hun ervaring minder maakt. Door deze feedback heb ik ook besloten om niks meer te veranderen aangezien ik zelf ook uiterst tevreden was over de site.</p>
+            <a href="https://www.youtube.com/watch?v=0vL6GxbOT8A" class="back-btn4">Video</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">      
+        <div class="box">
+            <img src="images/scotlogoblack.png">
+        </div>
+        <div class="box">
+            <h2>Laatste feedback</h2>
+            <p>Als laatste heb ik het geheel nog eens aan Joris laten zien om metname feedback op de documentatie te krijgen. Joris vond dat de documentatie al compleet en goed eruit zag maar raadde me aan om de dingen die ik onderzocht en gemaakt heb meer te koppelen aan de behoeftes van de doelgroep en om expliciet de onderzoeks methodes te vernoemen dus dat heb ik daarna nog aangepast.</p>
         </div>
     </div>
     <div class="row animated fade-down delay-200">
         <div class="box">
-            <img src="" alt="">
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Ik heb enorm veel geleerd over het starten van een bedrijf en wat daarbij komt kijken. Ik wil ook nog graag verder gaan met dit project na school en dit merk ook echt uitbouwen.</p>
         </div>
         <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="row animated fade-down delay-200">
-        <div class="box">
-            <h2></h2>
-            <p></p>
-        </div>
-        <div class="box">
-            <h2></h2>
-            <p></p>
+            <h2>Wat kon beter?</h2>
+            <p>Het onderzoek kon meer volgens de CMD methods. Deze heb ik laterna pas er tussen gepropt en ik had ook beter en gerichter onderzoek kunnen doen als ik deze methodes gevolgt had.</p>
         </div>
     </div>
 </div>
 <div class="next-box animated fade-down delay-200">
-    <a href="#" class="next">Volgende project</a>
+    <a href="projects.php" class="next">Terug naar projecten</a>
 </div>
 <script src="script.js"></script>    
 </body>
