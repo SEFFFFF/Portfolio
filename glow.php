54304ab9c964930fbb5f15d584e13bc545034f8b
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-06-17 12:42:29 +0200

    v1.3 Added Glow project page

---
+++ glow.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="temp dev.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel='icon' href='images/favicon.png' type='image/x-icon' >
+    <title>Glow Lightweek</title>
+</head>
+<body>
+<?php 
+    include 'include/header.php'
+?>
+<div class="top animated fade-down delay-200">
+    <div class="topbox">
+        <a href="projects.php" class="back-btn">Back</a>
+    </div>
+    <div class="topbox">
+        <h1>Glow Lightweek</h1>
+        <p>Als speciale opdracht tussen de proftaak en sprint X hebben we op een paar dagen tijd een product bedacht wat mensen beinvloed door licht.</p>
+    </div>
+</div>
+<div class="container">
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <img src="images/oskar.png" alt="iteraties">
+        </div>
+        <div class="box">
+            <h2>Idee</h2>
+            <p>Deze opdracht heb ik samen met Joep de Kock gemaakt. We begonnen door ideeen met elkaar te bespreken. Al snel kwam ik met het idee om een lamp te maken die een onuitstaanbaar licht gaat branden als er te hard in een ruimte gepraat wordt.</p>
+            <p>Dit idee beviel ons beide goed. Nu moesten we nog een naam hebben. Om de lamp echt een persoonlijkheid te geven hebben we gezocht naar een goede mensen naam. Joep kwam uiteindelijk met de naam "OSKAR" ook dit vonden wij goed passen. Ik heb deze naam even snel tot logo uitgewerkt en hier een mini style guide bij gemaakt, dit was gedaan in 10 minuten.</p>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <img src="images/story.png" alt="finished">
+        </div>
+        <div class="box">
+            <h2>Video</h2>
+            <p>Om ons idee werkelijkheid te maken hebben we een korte product video gemaakt. Hiervoor heb ik als eerste een storyboard geschetst om een goed idee te krijgen hoe we dit gingen aanpakken. Met de story board compleet hebben we besproken waar we dit gingen filmen, het leek ons het beste om dit te doen op Strijp-S omdat we dan ook gebruik konden maken van de ISSD en klasgenoten als figuranten.</p>
+            <p>Het opnemen van dit filmpje was zo gedaan. We hebben besloten dat ik de hoofdrol zou spelen en een paar klasgenoten de hangjeugd die mijn lezen voorstoren. Toen ik thuis kwam heb ik deze beelden gemonteerd in Premiere Pro ook dit was zo gefikst.</p>
+            <h3>Feedback</h3>
+            <p>Zowel het idee als het filmpje werd goed ontvangen tijdens de presentatie, we hebben helaas geen honorable mention gekregen maar wel nog complimenten voor het uitbeelden van ons idee. </p>
+            <a href="https://youtu.be/4eJdteumkM0" class="back-btn4">Video</a>
+        </div>
+    </div>
+    <div class="row animated fade-down delay-200">
+        <div class="box">
+            <h2>Wat heb ik geleerd?</h2>
+            <p>Ik heb voor het eerst een storyboard geschetst voor een video, dit zal ik als ik weer een soortgelijk project moet doen ook weer maken want ik vond dat dit het filmen een stuk makkelijker maakt.</p>
+        </div>
+        <div class="box">
+            <h2>Wat kon beter?</h2>
+            <p>Volgende keer wil ik meer aandacht besteden aan het monteren van de video omdat ik er steeds meer achter kom dat ik dit heel erg leuk vind.</p>
+        </div>
+    </div>
+</div>
+<div class="next-box animated fade-down delay-200">
+    <a href="portfolio website.php" class="next">Volgende project</a>
+</div>
+<script src="script.js"></script>    
+</body>
+</html>
